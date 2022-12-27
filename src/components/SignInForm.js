46f283d8ff81abb3a2a12a2ae0f23d0448426b43
Kacper <kacperhaha11@gmail.com>
2022-12-28 00:52:36 +0100

    fixed text typo, changed font sizes

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -6,13 +6,17 @@ const navigate = useNavigate();
 const { register, handleSubmit } = useForm();
 const onSubmit = data => LogIn(data.username, data.password, navigate);
 function LogIn(username, password, navigate) {
-    axios.post('http://localhost:80/api/auth/login', {
+    axios.post('http://localhost:80/api/auth/login',{
       email: username.toLowerCase() || username,
       password: password
-    })
+    }, {headers: { 
+      'Content-Type': 'application/json',
+  }}
+)
     .then(function (response) {
 
       if(response.status === 200) {
+        console.log(response)
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('user', JSON.stringify(response.data.user));
         navigate(`/`);
@@ -29,16 +33,16 @@ return (
         <span className="text-3xl font-bold text-center text-[#00a3ff]">Sign in</span>
         <form className="flex flex-col items-center justify-center mt-4 gap-2" onSubmit={handleSubmit(onSubmit)}>
           <div className="relative">
-            <input type="text" placeholder="username" id="username" className="placeholder:opacity-0 peer w-72 h-10 pt-2 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 text-[#00a3ff] font-bold text-lg focus:outline-none" {...register("username")} />
-            <label htmlFor="username" className="text-[#6e7680] select-none placeholder-opacity-0 absolute left-4 peer-focus:top-0 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-sm font-bold text-sm transition-all">Email</label>
+            <input type="text" placeholder="username" id="username" className="placeholder:opacity-0 peer w-72 h-10 pt-4 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 pr-4 text-[#00a3ff] font-bold text-base leading-5 pb-1 focus:outline-none" {...register("username")} />
+            <label htmlFor="username" className="absolute text-[#6e7680] select-none placeholder-opacity-0 top-1 left-4 peer-focus:top-1 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-xs font-bold text-xs transition-all">Email</label>
           </div>
           <div className="relative">
-            <input type="password" id="password" placeholder="Password" className="placeholder:opacity-0 peer w-72 h-10 pt-2 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 text-[#00a3ff] font-bold text-lg focus:outline-none" {...register("password")} />
-            <label htmlFor="password" className="absolute text-[#6e7680] placeholder-opacity-0 left-4 peer-focus:top-0 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-sm font-bold text-sm transition-all">Password</label>
+            <input type="password" id="password" placeholder="Password" className="placeholder:opacity-0 peer w-72 h-10 pt-4 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 pr-4 text-[#00a3ff] font-bold text-base leading-5 pb-1 focus:outline-none" {...register("password")} />
+            <label htmlFor="password" className="absolute text-[#6e7680] select-none placeholder-opacity-0 top-1 left-4 peer-focus:top-1 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-xs font-bold text-xs transition-all">Password</label>
           </div>
           <button className="w-72 h-10 bg-[#00a3ff] rounded-xl shadow-2xl pl-4 text-white font-bold text-xl focus:outline-none mt-4 hover:bg-[#0091e6] ">Sign in</button>
         </form>
-        <span> Need an account?<u onClick={() => { navigate(`/signup`); } }>Sign up</u></span>
+        <span> Need an account? <u onClick={() => { navigate(`/signup`); } }>Sign up</u></span>
       </div>
     </div>
     <div className="absolute h-screen w-screen z-[100] top-0 right-0 bg-black animate-FadeIn"></div>
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -29,20 +29,20 @@ const onSubmit = data => RegisterAccount(data.email, data.username, data.passwor
     <span className="text-3xl font-bold text-center text-[#00a3ff]">Sign up</span>
     <form className="flex flex-col items-center justify-center mt-4 gap-2" onSubmit={handleSubmit(onSubmit)}>
       <div className="relative">
-        <input type="text" name="email" placeholder="email" id="email" className="placeholder:opacity-0 peer w-72 h-10 pt-2 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 text-[#00a3ff] font-bold text-lg focus:outline-none"  {...register("email")} />
-        <label htmlFor="email" className="text-[#6e7680] placeholder-opacity-0 absolute left-4 peer-focus:top-0 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-sm font-bold text-sm transition-all">Email</label>
+        <input type="text" name="email" placeholder="email" id="email" className="placeholder:opacity-0 peer w-72 h-10 pt-4 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 pr-4 text-[#00a3ff] font-bold text-base leading-5 pb-1 focus:outline-none"  {...register("email")} />
+        <label htmlFor="email" className="absolute text-[#6e7680] select-none placeholder-opacity-0 top-1 left-4 peer-focus:top-1 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-xs font-bold text-xs transition-all">Email</label>
       </div>
       <div className="relative">
-        <input type="text" placeholder="username" id="username" className="placeholder:opacity-0 peer w-72 h-10 pt-2 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 text-[#00a3ff] font-bold text-lg focus:outline-none"  {...register("username")} />
-        <label htmlFor="username" className="text-[#6e7680] select-none placeholder-opacity-0 absolute left-4 peer-focus:top-0 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-sm font-bold text-sm transition-all">Username</label>
+        <input type="text" placeholder="username" id="username" className="placeholder:opacity-0 peer w-72 h-10 pt-4 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 pr-4 text-[#00a3ff] font-bold text-base leading-5 pb-1 focus:outline-none"  {...register("username")} />
+        <label htmlFor="username" className="absolute text-[#6e7680] select-none placeholder-opacity-0 top-1 left-4 peer-focus:top-1 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-xs font-bold text-xs transition-all">Username</label>
       </div>
       <div className="relative">
-        <input type="password" id="password" placeholder="Password" className="placeholder:opacity-0 peer w-72 h-10 pt-2 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 text-[#00a3ff] font-bold text-lg focus:outline-none"  {...register("password")} />
-        <label htmlFor="password" className="text-[#6e7680] placeholder-opacity-0 absolute left-4 peer-focus:top-0 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-sm font-bold text-sm transition-all">Password</label>
+        <input type="password" id="password" placeholder="Password" className="placeholder:opacity-0 peer w-72 h-10 pt-4 bg-[#f2f2f2] rounded-lg border-black border-[1px] pl-4 pr-4 text-[#00a3ff] font-bold text-base leading-5 pb-1 focus:outline-none"  {...register("password")} />
+        <label htmlFor="password" className="absolute text-[#6e7680] select-none placeholder-opacity-0 top-1 left-4 peer-focus:top-1 peer-placeholder-shown:top-[0.44rem] peer-placeholder-shown:text-xl peer-focus:text-xs font-bold text-xs transition-all">Password</label>
       </div>
       <button className="w-72 h-10 bg-[#00a3ff] rounded-xl shadow-2xl pl-4 text-white font-bold text-xl focus:outline-none mt-4 hover:bg-[#0091e6]">Sign up</button>
     </form>
-    <span> Already have an account? <u onClick={() => {navigate(`/signin`)}}>Sing in</u></span>
+    <span> Already have an account? <u onClick={() => {navigate(`/signin`)}}>Sign in</u></span>
   </div>
 </div>
   <div className="h-screen w-screen absolute z-[100] top-0 right-0 bg-black animate-FadeIn"></div>
